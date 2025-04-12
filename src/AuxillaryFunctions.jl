52a6449ae6ca0ed845a474d163a4a0cb9cd66d08
mtarchick13@gmail.com <mtarchick13@gmail.com>
2025-04-12 12:07:52 -0700

    Add photoreceptor model functions for current calculations

--- src/AuxillaryFunctions.jl
+++ src/AuxillaryFunctions.jl
@@ -9,3 +9,8 @@ H_inf(v, l, h) = v != 0 ? 1/(1+exp((v+l)/h)) : 0
 B_inf(v, l, h) = v > 0 ? 1/(1+exp((v+l)/h)) : 0
 
 #Below is a work in progress for a more complicated photoreceptor model
+J(cGMP, Jmax) = (Jmax*cGMP^3)/(cGMP^3 + 10^3) #CNG currents
+gL = 0.35 #Leakage conductance
+EL = -77.0 #Leakage potential
+ILeak(V) = gL*(V-EL)
+Iphoto(V, cGMP, Jmax) = -J(cGMP, Jmax)*(1.0 - exp((V-8.5)/17.0))
